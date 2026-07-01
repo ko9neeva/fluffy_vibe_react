bdbe97811fce082cc0789b15fe8064e799b83451
Katya Korneeva <trofimo-ekaterina@yandex.ru>
2026-07-01 18:31:18 +0300

    Fix router basename for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     } = useHoodies(products);
 
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Navbar />
             <AppRoutes
                 hoodies={hoodies}
